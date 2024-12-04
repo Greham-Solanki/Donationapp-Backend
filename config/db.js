aed18d08cd6d57331cbc5afad729e9a01fc4133f
Greham-Solanki <grehamsolanki91@gmail.com>
2024-12-04 17:31:33 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,29 +4,17 @@ const fs = require('fs');
 const connectDB = async () => {
     try {
         const dbURI = process.env.MONGODB_URI;
-        
-        if (!dbURI) {
-            console.error('MONGODB_URI is not defined');
-            process.exit(1);
-        }
-
-        console.log('Attempting to connect to:', dbURI);
-
         await mongoose.connect(dbURI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            ssl: true,
-            sslValidate: true,
-            sslCA: fs.readFileSync('/config/global-bundle.pem'),
-            retryWrites: false
+            tls: true, // Use TLS instead of SSL
+            tlsCAFile: './config/global-bundle.pem',
         });
         console.log('MongoDB connected successfully');
     } catch (error) {
         console.error('MongoDB connection error:', error.message);
-        console.error('Full error:', error);
         process.exit(1);
     }
 };
 
-// IMPORTANT: Use module.exports, not exports.connectDB
 module.exports = connectDB;
