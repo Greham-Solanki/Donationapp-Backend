d957b42199193d4200ada4a56be1fbde75ea2d34
Greham-Solanki <grehamsolanki91@gmail.com>
2024-12-05 08:46:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,13 @@ const notificationRoutes = require('./routes/notificationRoutes');
 const app = express();
 connectDB(); // Connect to the database
 
+app.get('/health', (req, res) => {
+  res.status(200).json({ 
+    status: 'healthy', 
+    uptime: process.uptime() 
+  });
+});
+
 app.use((req, res, next) => {
   console.log(`Received request: ${req.method} ${req.url}`);
   next();
