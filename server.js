8c6fdc8a4501d407425e74dd3cafa5ca8518828c
Greham-Solanki <grehamsolanki91@gmail.com>
2024-12-05 08:54:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
 });
 // CORS options
 const corsOptions = {
-  origin: 'http://frontend-alb-1907579939.us-east-1.elb.amazonaws.com',
+  origin: 'http://frontend-alb-1907579939.us-east-1.elb.amazonaws.com:3000',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
