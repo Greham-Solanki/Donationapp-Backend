8352b2787dc69755a03296ce58fcc3c19b6d7488
Greham-Solanki <grehamsolanki91@gmail.com>
2026-03-16 01:55:54 -0400

    Add env deployment

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const notificationRoutes = require('./routes/notificationRoutes');
 const app = express();
 connectDB(); 
 
-// Request logging middleware
+// Request logging middleware  :)
 app.use((req, res, next) => {
   console.log(`📨 ${req.method} ${req.url}`);
   next();
